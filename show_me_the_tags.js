f3aa3043da5c3cb628541fd1648862e4a42044c4
Develop.Kim <developkim@me.com>
2020-08-26 03:00:52 +0900

    Update show_me_the_tags.js

--- show_me_the_tags.js
+++ show_me_the_tags.js
@@ -27,6 +27,7 @@ function prepend_infos(page_title, description, tags){
   oDiv.innerHTML = strInnerHTML;
   oDiv.style.cssText = "color: blue; border: 1px solid black";
   var oDivInfo = document.querySelectorAll('#wrap > div > div.prd_detail_basic > div.info')[0];
+  if( oDivInfo == undefined ) oDivInfo = document.querySelectorAll('#content > div > div.prd_detail_basic > div.info')[0];
   oDivInfo.prepend(oDiv);
 }
 
